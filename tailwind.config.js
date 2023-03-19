91f26366bf9cafa676192b4bb7ab81c2e144aa02
John Silver <53310608+LongJohnSilver1504@users.noreply.github.com>
2023-03-19 12:21:07 +0100

    feat: allow customize antd components with tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ const { fontFamily } = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	important: true,
 	darkMode: ['class'],
 	content: [
 		'./app/**/*.{js,ts,jsx,tsx}',
